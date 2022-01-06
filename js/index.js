80af5e9ab6324440aabd2c1391b35fe9b02056bc
jjohnjbell <83473305+jjohnjbell@users.noreply.github.com>
2022-01-06 08:31:39 -0500

    commit on MailPacNew

--- js/index.js
+++ js/index.js
@@ -1,53 +1,50 @@
 // window.onload = () => {
-const results = document.getElementById("results")
+const resultsEl = document.getElementById("results")
 const getLinksBtn = document.getElementById("myBtn")
 const deleteBtn = document.getElementById("deleteBtn-El")
 
 const bookmarks = {
-    myLinks: [],
-    myHostName: []
+    myLinks: "",
+    myHostName: ""
 }
 
-
+//render(bookmarks)
 
 getLinksBtn.addEventListener("click", function (e) {
     e.preventDefault()
-
-
 
     if (localStorage.getItem('myMarks') === null) {
 
         chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
             let bookmarkObjectArray = []
+            bookmarks.myLinks = tabs[0].url
+            bookmarks.myHostName = tabs[0].title
+            bookmarkObjectArray.push(bookmarks)
+            localStorage.setItem("myMarks", JSON.stringify(bookmarkObjectArray))
+        })
+
+    } else {
+        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+            let bookmarkObjectArray = JSON.parse(localStorage.getItem("myMarks"))
             bookmarks.myLinks.push(tabs[0].url)
             bookmarks.myHostName.push(tabs[0].title)
             bookmarkObjectArray.push(bookmarks)
-            localStorage.setItem("myMarks",JSON.stringify(bookmarkObjectArray))
+            localStorage.setItem("myMarks", JSON.stringify(bookmarkObjectArray))
+
         })
 
-        } else {
-            chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-                let bookmarkObjectArray = JSON.parse(localStorage.getItem("myMarks"))
-                bookmarks.myLinks.push(tabs[0].url)
-                bookmarks.myHostName.push(tabs[0].title)
-                bookmarkObjectArray.push(bookmarks)
-                localStorage.setItem("myMarks",JSON.stringify(bookmarkObjectArray))
+    }
 
-            })
+    render(bookmarkObjectArray)
+})
 
-        }
-
-       // render(bookmarks)
-    })
-
-// function render(bookmarker) {
-//    results.innerHTML = ""
-//     for (let i = 0; i < bookmarks.length; i++) {
-//            results.innerHTML += `<div><h3> ${bookmarks.myHostName[i]} 
-//            <a class="btn btn-default" target="_blank"  href =" ' + ${bookmarks.myLinks[i]} + ' "> Visit </a>
-//            </h3></div>`
-//     }
-// }
+function render(bookmarker) {
+    for (let i = 0; i < bookmarker.length; i++) {
+        resultsEl.innerHTML = `<h3>
+                            ${bookmarkObjectArray[i]}
+                            </h3>`
+    }
+}
 
 
 deleteBtn.addEventListener("click", function () {
