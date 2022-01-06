f45961bb9229276e47308343a503598f8e0c792e
jjohnjbell <83473305+jjohnjbell@users.noreply.github.com>
2022-01-06 08:35:28 -0500

    commit on MailPacNew

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@
 const resultsEl = document.getElementById("results")
 const getLinksBtn = document.getElementById("myBtn")
 const deleteBtn = document.getElementById("deleteBtn-El")
+const bookmarkObjectArray = []
 
 const bookmarks = {
     myLinks: "",
@@ -16,7 +17,6 @@ getLinksBtn.addEventListener("click", function (e) {
     if (localStorage.getItem('myMarks') === null) {
 
         chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-            let bookmarkObjectArray = []
             bookmarks.myLinks = tabs[0].url
             bookmarks.myHostName = tabs[0].title
             bookmarkObjectArray.push(bookmarks)
@@ -25,7 +25,7 @@ getLinksBtn.addEventListener("click", function (e) {
 
     } else {
         chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-            let bookmarkObjectArray = JSON.parse(localStorage.getItem("myMarks"))
+            bookmarkObjectArray = JSON.parse(localStorage.getItem("myMarks"))
             bookmarks.myLinks.push(tabs[0].url)
             bookmarks.myHostName.push(tabs[0].title)
             bookmarkObjectArray.push(bookmarks)
