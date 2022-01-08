1e815b3d7d340f002d221b1be10afed2d7be0762
Joshua Bell <83473305+jjohnjbell@users.noreply.github.com>
2022-01-08 12:29:28 -0500

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -2,7 +2,9 @@
 const resultsEl = document.getElementById("results")
 const getLinksBtn = document.getElementById("myBtn")
 const deleteBtn = document.getElementById("deleteBtn-El")
-let bookmarkObjectArray = [] 
+const showLinksBtn = document.getElementById("showLinks-El")
+let storageContent = JSON.parse(localStorage.getItem("myMarks"))
+let bookmarkObjectArray = []
 let onloaderEl = ""
 
 const bookmarks = {
@@ -11,7 +13,9 @@ const bookmarks = {
 }
 
 
-
+if ( storageContent!= null) {
+    render(storageContent)
+}
 
 
 getLinksBtn.addEventListener("click", function (e) {
@@ -20,37 +24,46 @@ getLinksBtn.addEventListener("click", function (e) {
     if (localStorage.getItem('myMarks') === null) {
 
         chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+            let url = new URL(tabs[0].url)
             bookmarks.myLinks = tabs[0].url
-            bookmarks.myHostName = tabs[0].title
+            bookmarks.myHostName = url.hostname
             bookmarkObjectArray.push(bookmarks)
             localStorage.setItem("myMarks", JSON.stringify(bookmarkObjectArray))
             render(bookmarkObjectArray)
+
         })
 
     } else {
         chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+            let url = new URL(tabs[0].url)
             bookmarkObjectArray = JSON.parse(localStorage.getItem("myMarks"))
-            bookmarks.myLinks=tabs[0].url
-            bookmarks.myHostName=tabs[0].title
+            bookmarks.myLinks = tabs[0].url
+            bookmarks.myHostName = url.hostname
             bookmarkObjectArray.push(bookmarks)
             localStorage.setItem("myMarks", JSON.stringify(bookmarkObjectArray))
             render(bookmarkObjectArray)
+
 
         })
 
     }
 
-    
+
 })
 
-// and ${bookmarker[i].myLinks}
+
+
 function render(bookmarker) {
     for (let i = 0; i < bookmarker.length; i++) {
         resultsEl.innerHTML += `<h5><div class="listEl">
-                            ${bookmarker[i].myHostName}` + ` <a class="" href='${bookmarker[i].myLinks}'>Visit</a>` + `<button class="btn btn-alert"> Delete </button>
+                            ${bookmarker[i].myHostName}` + ` <a href="${bookmarker[i].myLinks}" target = "_blank">Visit</a> ` +
+            `<button id="copy-El" class="btn-secondary"> COPY</button>` + `<button id = "deleteURL" class="btn btn-alert"> Delete </button>
                             </div></h5>`
+
     }
+
 }
+
 
 
 deleteBtn.addEventListener("click", function () {
