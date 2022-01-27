2d03f1b0ecf0a6167769459310a44dbdae2afe68
jjohnjbell <83473305+jjohnjbell@users.noreply.github.com>
2022-01-27 15:38:34 -0500

    commit on mailpacnew

--- js/index.js
+++ js/index.js
@@ -13,15 +13,16 @@ let onloaderEl = ""
 
 const bookmarks = {
     myLinks: "",
-    myHostName: "",
     trueLink: ""
 }
 
-// if (storageContent === null) {
-//     showLinksBtn.style = "display:none"
-// }
+if (storageContent === null) {
+    showLinksBtn.style = "display:none"
+}
+
 
 getLinksBtn.addEventListener("click", getLinks)
+
 function getLinks(e) {
     e.preventDefault()
 
@@ -31,17 +32,14 @@ function getLinks(e) {
             let url = toString(new URL(tabs[0].url))
             let newUrl = tabs[0].url.substring(12, 40) + "..."
 
-            // bookmarks.myLinks = tabs[0].url
+
             bookmarks.myLinks = newUrl
-            bookmarks.myHostName = url.hostname
             bookmarks.trueLink = tabs[0].url
             bookmarkObjectArray.push(bookmarks)
             localStorage.setItem("myMarks", JSON.stringify(bookmarkObjectArray))
 
             // popUp.style = "visibility: display"
             popUp.style = "visibility:visible"
-
-
 
             setTimeout(() => popUp.style = "visibility:hidden", 1200)
             showLinksBtn.style = "display=visible"
@@ -53,11 +51,9 @@ function getLinks(e) {
             let url = toString(new URL(tabs[0].url))
             let newUrl = tabs[0].url.substring(12, 40) + "..."
 
-            // bookmarks.myLinks = tabs[0].url
             bookmarkObjectArray = JSON.parse(localStorage.getItem("myMarks"))
             bookmarks.myLinks = newUrl
             bookmarks.trueLink = tabs[0].url
-            bookmarks.myHostName = url.hostname
             bookmarkObjectArray.push(bookmarks)
             localStorage.setItem("myMarks", JSON.stringify(bookmarkObjectArray))
             popUp.style = "visibility:visible"
@@ -66,73 +62,46 @@ function getLinks(e) {
             setTimeout(() => popUp.style = "visibility:hidden", 1200)
 
             showLinksBtn.style = "display=visible"
-
         })
-
+    }
     }
 
+    showLinksBtn.addEventListener("click", function (e) {
+        e.preventDefault()
 
-    
-}
+        //Store LocalStorage Content
+        let lsContent = JSON.parse(localStorage.getItem("myMarks"))
+        for (let i = 0; i < lsContent.length; i++) {
 
-// showLinksBtn.addEventListener("click", function (e) {
-//     e.preventDefault()
+            let copyBtn = document.createElement('button')
+            copyBtn.className = "resultSetBtn"
+            copyBtn.innerHTML = "Copy"
+            copyBtn.addEventListener("click", function () {
+                navigator.clipboard.writeText(lsContent[i].trueLink)
+            })
 
-//     //Store LocalStorage Content
-//     let lsContent = JSON.parse(localStorage.getItem("myMarks"))
-//     for (let i = 0; i < lsContent.length; i++) {
+            let deleteBtn = document.createElement('button')
+            deleteBtn.className = "resultSetBtn"
+            deleteBtn.innerHTML = "Delete"
 
-//         let copyBtn = document.createElement('button')
-//         copyBtn.className = "resultSetBtn"
-//         copyBtn.innerHTML = "Copy"
-//         copyBtn.addEventListener("click", function () {
-//             navigator.clipboard.writeText(lsContent[i].trueLink)
-//         })
+            let newDiv = document.createElement('div')
+            newDiv.id = "resultSetDiv"
+            newDiv.innerHTML += `<a href="${lsContent[i].myLinks}"target = "_blank">${lsContent[i].myLinks}</a>`
 
-//         let deleteBtn = document.createElement('button')
-//         deleteBtn.className = "resultSetBtn"
-//         deleteBtn.innerHTML = "Delete"
+            newDiv.appendChild(deleteBtn)
+            newDiv.appendChild(copyBtn)
 
-//         let newDiv = document.createElement('div')
-//         newDiv.id = "resultSetDiv"
-//         newDiv.innerHTML += `<a href="${lsContent[i].myLinks}"target = "_blank">${lsContent[i].myLinks}</a>`
+            resultsEl.appendChild(newDiv)
+            console.log(lsContent[i].myLinks)
 
-//         newDiv.appendChild(deleteBtn)
-//         newDiv.appendChild(copyBtn)
+        }
 
-//         resultsEl.appendChild(newDiv)
-//         console.log(lsContent[i].myLinks)
-
-//     }
-  
-//     // resultsEl.innerHTML += lsContent[0].myLinks
-//     // console.log(lsContent.length)
-// })
-
-
-
-
-
-
-showLinksBtn.addEventListener("click", function (e) {
-    e.preventDefault()
-
-        console.log(bookmarkObjectArray[0])
 
     })
 
-  
-    // resultsEl.innerHTML += lsContent[0].myLinks
-    // console.log(lsContent.length)
-//})
-
-
-
-
-
-function clearChild() {
-    document.getElementById("resultSetDiv").remove()
-}
-deleteBtn.addEventListener("click", function () {
-    localStorage.clear()
-})
+    function clearChild() {
+        document.getElementById("resultSetDiv").remove()
+    }
+    deleteBtn.addEventListener("click", function () {
+        localStorage.clear()
+    })
