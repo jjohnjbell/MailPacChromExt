36ded1ecf2bf74440fdfcc7681b8321a4f0a31c8
jjohnjbell <83473305+jjohnjbell@users.noreply.github.com>
2022-02-08 21:09:40 -0500

    commit on mailpacnew

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+window.onload = showLinks()
+
 //Connect to Div's
 let resultsEl = document.getElementById("results")
 
@@ -6,7 +8,7 @@ const popUp = document.getElementById("popUp")
 //Connect to Button Elements
 const getLinksBtn = document.getElementById("myBtn-El")
 const deleteBtn = document.getElementById("deleteBtn-El")
-const showLinksBtn = document.getElementById("showLinks-El")
+//const showLinksBtn = document.getElementById("showLinks-El")
 
 //Create Local Storage Variable
 //let storageContent = JSON.parse(localStorage.getItem("myMarks"))
@@ -25,31 +27,60 @@ const bookmarks = {
     trueLink: ""
 }
 
-//Check if Local Storage is empty, if so it hides the Show Links Button
-hideShowBtn()
+// //Check if Local Storage is empty, if so it hides the Show Links Button
+// hideShowBtn()
 
-function hideShowBtn() {
-    if (localStorage.getItem("myMarks") === null) {
-        showLinksBtn.style = "display:none"
-    }
-}
+// function hideShowBtn() {
+//     if (localStorage.getItem("myMarks") === null) {
+//         showLinksBtn.style = "display:none"
+//     }
+// }
 
 //Create function to Create Bookmark Objects and Store them in Local Storage
 getLinksBtn.addEventListener("click", getLinks)
 
-// e.preventDefault()
+
+
+function showLinks() {
+
+    //Get content from Local Storage
+    let bookmarkObjectArray = []
+    bookmarkObjectArray = JSON.parse(localStorage.getItem("myMarks"))
+    document.getElementById("results").innerHTML = ""
+
+    if(localStorage.getItem("myMarks") != null){
+    //Render Locasl Storage Items to Screen
+    for (let i = 0; i < bookmarkObjectArray.length; i++) {
+
+        let copyBtn = document.createElement('button')
+        copyBtn.className = "resultSetBtn"
+        copyBtn.innerHTML = "Copy"
+        copyBtn.addEventListener("click", function () {
+            navigator.clipboard.writeText(bookmarkObjectArray[i].trueLink)
+        })
+
+
+        let newDiv = document.createElement('div')
+        newDiv.id = "resultSetDiv"
+        newDiv.innerHTML = `<a href="${bookmarkObjectArray[i].myLinks}"target = "_blank">${bookmarkObjectArray[i].myLinks}</a>`
+        newDiv.appendChild(copyBtn)
+
+        document.getElementById("results").appendChild(newDiv)
+
+    }
+}
+}
+
 function getLinks() {
 
-    let bookmarkObjectArray = []
-    //If Local Storage is empty, Assign Bookmark values and push to Local Storage
-    if (localStorage.getItem("myMarks") === null) {
+    //Select current Chrome Tab
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        let bookmarkObjectArray = []
+        let newUrl = tabs[0].url.substring(0, 40) + "..."
 
-        //Select current Chrome Tab
-        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-            //Convert URL Object to String to enable the use of the Substring method
-            // use this to get hostname from URL Object "let url = new URL(tabs[0].url)....url.hostname
-            let newUrl = tabs[0].url.substring(0, 40) + "..."
-
+        //If Local Storage is empty, Assign Bookmark values and push to Local Storage
+        if (localStorage.getItem("myMarks") === null) {
+            resultsEl.innerHTML = ""
             //Assign values to the Bookmark Object
             bookmarks.myLinks = newUrl
             bookmarks.trueLink = tabs[0].url
@@ -58,15 +89,11 @@ function getLinks() {
 
             // popUp.style = "visibility: display"
             popDisplay("Link Grabbed")
+            //showLinksBtn.style = "display=visible"
 
-
-
-            showLinksBtn.style = "display=visible"
-
-            resultsEl.innerHTML = ""
-
-            //NEW SHOW LINK FUNCTION
-            for (let i = 0; i < bookmarkObjectArray.length; i++) {
+            //Render Local Storage Items to Screen
+            let lsContent = JSON.parse(localStorage.getItem("myMarks"))
+            for (let i = 0; i < lsContent.length; i++) {
 
                 let copyBtn = document.createElement('button')
                 copyBtn.className = "resultSetBtn"
@@ -84,23 +111,11 @@ function getLinks() {
                 resultsEl.appendChild(newDiv)
             }
 
-
-        })
-        //END SHOW LINK FUNCTION
-
-
-    } else {
-        //Select current Chrome Tab
-        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-
-            //Convert URL  to String to enable the use of the Substring method
-            let actualLink = tabs[0].url
-            let newUrl = tabs[0].url.substring(0, 40) + "..."
-            let bookmarkObjectArray = []
-
-
+        } else {
+            resultsEl.innerHTML = ""
             //Get content from Local Storage
             bookmarkObjectArray = JSON.parse(localStorage.getItem("myMarks"))
+
             //Assign values to the Bookmark Object
             bookmarks.myLinks = newUrl
             bookmarks.trueLink = tabs[0].url
@@ -108,19 +123,10 @@ function getLinks() {
             localStorage.setItem("myMarks", JSON.stringify(bookmarkObjectArray))
 
             popDisplay("Link Grabbed")
+          //  showLinksBtn.style = "display=visible"
 
-            showLinksBtn.style = "display=visible"
-
-            // for (let i = 0; i < bookmarkObjectArray.length; i++) {
-            //     if ((bookmarkObjectArray[i].trueLink === actualLink)&&(actualLink===tabs[0].url)) {
-            //         popDisplay("Link Already Grabbed")
-
-            //     } else {
-
-
-            //NEW SHOW LINK FUNCTION
+            //Render Locasl Storage Items to Screen
             for (let i = 0; i < bookmarkObjectArray.length; i++) {
-
 
                 let copyBtn = document.createElement('button')
                 copyBtn.className = "resultSetBtn"
@@ -138,34 +144,15 @@ function getLinks() {
                 resultsEl.appendChild(newDiv)
 
             }
-
-        })
-
-    }
+        }
+    })
 
 }
-//END SHOW LINK FUNCTION
-
-
-
-
-
-
-
-
-
 
 //Create function to remove the Div's that store the Result Set
 function clearChild() {
     document.getElementById("resultSetDiv").remove()
 }
-
-
-
-
-
-
-
 
 //Create Function to clear Local Storage
 deleteBtn.addEventListener("click", function () {
